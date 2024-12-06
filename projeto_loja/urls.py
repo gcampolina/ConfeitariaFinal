89e5750cff605a7d692751abf7da766f2904b6ef
Gabriel Campolina <gcampolinap@gmail.com>
2024-12-06 12:12:34 -0300

    att urls

--- projeto_loja/urls.py
+++ projeto_loja/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app_loja import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', include('app_loja.urls')),
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('produtos/', views.produtos, name='produtos'),
